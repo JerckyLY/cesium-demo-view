ff555c54874b582fc881fbd452d81215928b7b67
jercky <1743877040@qq.com>
2021-07-08 18:11:17 +0800

    5. 帧率-readme

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
  3. 自定义Popup框 <a href='https://jercky.top/2020/10/22/Cesium%E5%85%A5%E9%97%A8-4/' target="_blank" >博客地址</a>   
  4. 点线面缓冲区 <a href='https://jercky.top/2021/03/23/Cesium%E5%85%A5%E9%97%A8-6/' target="_blank" >博客地址</a>
  5. 方位角测量 <a href='https://jercky.top/2021/03/26/Cesium%E5%85%A5%E9%97%A8-7/' target="_blank" >博客地址</a>
+ 6. 自定义帧率 <a href='https://jercky.top/2021/06/23/Cesium%E5%85%A5%E9%97%A8-8/' target="_blank" >博客地址</a>
---
+++ Utils/CesiumFPSUtil.js
@@ -0,0 +1,73 @@
+
+
+function getTimestamp() {
+  var getTime;
+
+  if (
+    typeof performance !== "undefined" &&
+    typeof performance.now === "function" &&
+    isFinite(performance.now())
+  ) {
+    getTime = function () {
+      return performance.now();
+    };
+  } else {
+    getTime = function () {
+      return Date.now();
+    };
+  }
+  return getTime();
+}
+class CesiumFPSUtil {
+  constructor(containerId) {
+    this.id = containerId || "fpsdiv";
+    this._lastFpsSampleTime = getTimestamp();
+    this._lastMsSampleTime = getTimestamp();
+    this._fpsFrameCount = 0;
+    this._msFrameCount = 0;
+    this.createDiv();
+  }
+
+  createDiv() {
+    const parent = document.getElementById(this.id);
+    const fpsDiv = document.createElement("div");
+    fpsDiv.className = "info-content";
+
+    this._fpsText = document.createElement("span");
+    this._msText = document.createElement("span");
+    fpsDiv.append(this._fpsText);
+    fpsDiv.append(this._msText);
+
+    parent.append(fpsDiv);
+  }
+
+
+  update() {
+    let time = getTimestamp();
+    this._fpsFrameCount++;
+    let updateDisplay = true;
+    let fpsElapsedTime = time - this._lastFpsSampleTime;
+    if (fpsElapsedTime > 1000) {
+      var fps = "N/A";
+      if (updateDisplay) {
+        fps = ((this._fpsFrameCount * 1000) / fpsElapsedTime) | 0;
+      }
+
+      this._fpsText.innerText = fps + " FPS";
+      this._lastFpsSampleTime = time;
+      this._fpsFrameCount = 0;
+    }
+    this._msFrameCount++;
+    let msElapsedTime = time - this._lastMsSampleTime;
+    if (msElapsedTime > 200) {
+      let ms = "N/A";
+      if (updateDisplay) {
+        ms = (msElapsedTime / this._msFrameCount).toFixed(2);
+      }
+
+      this._msText.innerText = ms + " MS";
+      this._lastMsSampleTime = time;
+      this._msFrameCount = 0;
+    }
+  }
+}
